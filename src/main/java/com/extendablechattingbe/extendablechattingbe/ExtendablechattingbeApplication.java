c96c4ad5b514f831eadb71190287f2311c13eca2
jihwan <sgh002400@naver.com>
2022-08-23 23:20:38 +0900

    first commit

---
+++ src/main/java/com/extendablechattingbe/extendablechattingbe/ExtendablechattingbeApplication.java
@@ -0,0 +1,13 @@
+package com.extendablechattingbe.extendablechattingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendablechattingbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendablechattingbeApplication.class, args);
+	}
+
+}
