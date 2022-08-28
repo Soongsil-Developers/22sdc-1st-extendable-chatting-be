501f3bd9d213c7c0bef0761e532070ecabb7bfa0
Eundms <dms011221@gmail.com>
2022-08-28 15:16:29 +0900

    add(config): related to swagger

---
+++ src/main/java/com/extendablechattingbe/extendablechattingbe/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.extendablechattingbe.extendablechattingbe.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(new Info().title("Extendable_Chatting API")
+                .description("Extendable_Chatting API 명세서 입니다.")
+                .version("v0.0.1"));
+    }
+}
+
