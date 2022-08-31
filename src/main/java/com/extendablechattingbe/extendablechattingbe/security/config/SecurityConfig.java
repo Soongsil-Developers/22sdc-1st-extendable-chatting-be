fe728c2d97084479b217bee227f1535e743e95de
hos05020 <hos05020@naver.com>
2022-08-31 21:39:55 +0900

    임시데이터 추가

--- src/main/java/com/extendablechattingbe/extendablechattingbe/security/config/SecurityConfig.java
+++ src/main/java/com/extendablechattingbe/extendablechattingbe/security/config/SecurityConfig.java
@@ -19,6 +19,9 @@ public class SecurityConfig {
     }
 
 
+
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests((auth)->{
