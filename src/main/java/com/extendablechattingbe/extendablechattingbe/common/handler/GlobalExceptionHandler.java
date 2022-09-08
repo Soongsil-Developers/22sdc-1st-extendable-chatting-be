c0962c67931cff5c84114d849d7a292e2a97c84b
Eundms <dms011221@gmail.com>
2022-09-09 08:28:08 +0900

    refactor(exception): delete ApiResponse Instead of ApiResponse, use ResponseEntity and ResponseMessage

--- src/main/java/com/extendablechattingbe/extendablechattingbe/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/extendablechattingbe/extendablechattingbe/common/handler/GlobalExceptionHandler.java
@@ -2,8 +2,9 @@ package com.extendablechattingbe.extendablechattingbe.common.handler;
 
 import static com.extendablechattingbe.extendablechattingbe.common.ResponseMessage.INTERNAL_SERVER_ERROR;
 
+import com.extendablechattingbe.extendablechattingbe.common.ResponseMessage;
 import com.extendablechattingbe.extendablechattingbe.common.error.ApiException;
-import com.extendablechattingbe.extendablechattingbe.dto.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,15 +12,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    protected ApiResponse<Object> handleException(final Exception e) {
+    protected ResponseEntity<ResponseMessage> handleException(final Exception e) {
         e.printStackTrace();
-        return ApiResponse.error(INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR.getStatus()).body(INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ApiException.class)
-    protected ApiResponse<Object> handleApiException(final ApiException e) {
+    protected ResponseEntity<ResponseMessage> handleApiException(final ApiException e) {
         e.printStackTrace();
-        return ApiResponse.error(e.getError());
+        ResponseMessage error=e.getError();
+        return ResponseEntity.status(error.getStatus()).body(error);
     }
 
 }
